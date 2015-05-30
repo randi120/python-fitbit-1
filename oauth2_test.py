b282927d5563b8fa09236b088c1bf8b0640781dc
Randi Cabezas <rcabezas@csail.mit.edu>
2015-05-30 18:02:08 -0400

    removing my test script

--- oauth2_test.py
+++
@@ -1,8 +0,0 @@
-
-import unittest
-from fitbit_tests.test_auth import Auth2Test
-
-
-if __name__ == "__main__":
-	suite = unittest.TestLoader().loadTestsFromTestCase(Auth2Test)
-	unittest.TextTestRunner(verbosity=2).run(suite)
